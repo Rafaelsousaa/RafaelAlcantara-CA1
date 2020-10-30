81f15e94acc0393610cab78ae08e0e692e8411ae
Rafaelsousaa <62604588+Rafaelsousaa@users.noreply.github.com>
2020-10-30 12:53:00 +0000

    The header
    in this parte of code i created a connection betewen the pages and give them the header

---
+++ Header.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<meta charset="utf-8">
+  <link rel="stylesheet" href="assets/css/css/bootstrap.css">
+  <link rel="stylesheet" href="assets/css/css/style.css">
+  <link rel="stylesheet" href="css/main.css">	
+
+</head>
+<body> 
+
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#"></a>   
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+        <a class="nav-link" href="main.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Page1.php">Page 1</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a class="nav-link" href="Page2.php">Page 2</a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" href="testando.php">Page2</a>
+        </li>
+       
+        
+      </ul>
+      <form class="form-inline mt-2 mt-md-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  
