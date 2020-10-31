4fdb5dd50cd9365237581e79731c8db3bd98c4e3
Rafaelsousaa <62604588+Rafaelsousaa@users.noreply.github.com>
2020-10-31 10:38:53 +0000

    home page
    in my home page i add the header by using include, than i added a photo and personal text about myself

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include('Header.php'); ?>
+<div class = "text">
+    <h1><strong>Rafael Alcantara de sousa</strong></h1>
+    <h2>Student Number - 2019335</h2>
+    <h3>Bachelor of science in information technology - 2nd Year</h3>
+    <h4>Web Development</h4>
+</div>
+<div class="space">
+
+</div>
+
+<?php include('Footer.php'); ?>
