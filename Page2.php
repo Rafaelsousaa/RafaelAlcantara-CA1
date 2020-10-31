8f6900d2ba9651403a41f4b129aab997b4a9c344
Rafaelsousaa <62604588+Rafaelsousaa@users.noreply.github.com>
2020-10-31 10:36:22 +0000

    page 2
    i add an array called country than i  used a if else stament and foreach for get the country and the capital inside that array country

--- Page2.php
+++ Page2.php
@@ -1,20 +1,45 @@
-<?php include('Header.php');?>
-<link rel="stylesheet" href="css/form.css"> 
+<?php include('Header.php'); ?>
 
-<body class="img">
+<?php
+$country = array(
+    "Italy" => "Rome", "Luxembourg" => "Luxembourg", "Belgium" => "Brussels", "Denmark" => "Copenhagen",
+    "Finland" => "Helsinki", "France" => "Paris", "Slovakia" => "Bratislava", "Slovenia" => "Ljubljana", "Germany" => "Berlin",
+    "Greece" => "Athens", "Ireland" => "Dublin", "Netherlands" => "Amsterdam", "Portugal" => "Lisbon", "Spain" => "Madrid",
+    "Sweden" => "Stockholm", "United Kingdom" => "London", "Cyprus" => "Nicosia", "Lithuania" => "Vilnius", "Czech Republic" => "Prague",
+    "Estonia" => "Tallin", "Hungary" => "Budapest", "Latvia" => "Riga", "Malta" => "Valetta", "Austria" => "Vienna", "Poland" => "Warsaw"
 
-<div class="form">
-<form action="Form.php" method = "POST">
-<strong>Put a Country</strong><br>
-<input type="text" name="fristname"><br>
-<input type="submit" value ="submit">
+); ?>
+<form action="Page2.php" method="POST">
+    <select name="x" id="">
+        <option value="" disabled selected> Pick a Country</option>
+        <?php
+
+        foreach ($country as $x => $x_value) {
+            echo "<option value='$x'>$x</option>";
+        }
+        ?>
+
+        <input type="submit" name="submit" value="choose">
+    </select>
+
+
 </form>
-</div>
-</body>
-	
-	
+<?php
 
-</body>
+if (isset($_POST['submit'])) {
 
-</html>
-</header>
+    foreach ($country as $x => $x_value) {
+
+        if ($_POST['x'] == $x) {
+            $can = $x;
+            $cap = $x_value;
+        }
+    }
+    echo '<h1>The country choosen is : ' . $can . '</h1>';
+    echo '<h2>The capital is : ' . $cap. '</h2>';
+}
+
+
+?>
+<div class="space2"></div>
+<?php include('Footer.php'); ?>
