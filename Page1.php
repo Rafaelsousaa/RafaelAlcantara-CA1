870dcd98ece97e68d1038f950d13b3a7d4782640
Rafaelsousaa <62604588+Rafaelsousaa@users.noreply.github.com>
2020-10-31 10:37:21 +0000

    table
    for this parte i deleted few pieces that was mistake than i stated to add my array inside my table by using foreach

--- Page1.php
+++ Page1.php
@@ -1,39 +1,30 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<?php include('Header.php');?>
-	<head>
-		<meta charset="UTF-8">
-		<title>CAWEB</title>
-		<link rel="stylesheet" href="css/page1.css"> 
-   <style>
-         table{
-            table-layout:auto;
-            width:90%;
-            margin :100px 20px 50px 200px;
-            padding: 10px 15px;
-        } 
-   </style>
-      
-    </head>
+<?php include('Header.php'); ?>
 
-	<body>
-        
-       
-    
-      
-        <table>
-            <tr><th>Country</th><th>Capital</th></tr>
-     
-    <?php
-    $country = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen",
-    "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin",
-    "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", 
-    "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague",
-    "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga","Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ; 
 
-    foreach($country as $x => $x_value){
 
-        echo "<tr><td>$x</td><td>$x_value</td></tr>";
-    }
-        
-    ?>        </table>
+<table class="table table-striped table-dark">
+    <thead>
+        <tr>
+            <th scope="col">Country</th>
+            <th scope="col">Capital</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $country = array(
+            "Italy" => "Rome", "Luxembourg" => "Luxembourg", "Belgium" => "Brussels", "Denmark" => "Copenhagen",
+            "Finland" => "Helsinki", "France" => "Paris", "Slovakia" => "Bratislava", "Slovenia" => "Ljubljana", "Germany" => "Berlin",
+            "Greece" => "Athens", "Ireland" => "Dublin", "Netherlands" => "Amsterdam", "Portugal" => "Lisbon", "Spain" => "Madrid",
+            "Sweden" => "Stockholm", "United Kingdom" => "London", "Cyprus" => "Nicosia", "Lithuania" => "Vilnius", "Czech Republic" => "Prague",
+            "Estonia" => "Tallin", "Hungary" => "Budapest", "Latvia" => "Riga", "Malta" => "Valetta", "Austria" => "Vienna", "Poland" => "Warsaw"
+        );
+
+        foreach ($country as $x => $x_value) {
+
+            echo "<tr><td>$x</td><td>$x_value</td></tr>";
+        }
+
+        ?>
+    </tbody>
+</table>
+<?php include('Footer.php'); ?>
